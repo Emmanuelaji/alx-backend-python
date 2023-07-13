ea7ac1664085b7c6e67f5aeeb6268e74be393329
EMMANUEL AJI AGBO <31956253+Emmanuelaji@users.noreply.github.com>
2023-07-13 02:42:44 +0100

    Update 4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
-'''Task 4's module.
-'''
-import asyncio
+""" Task 4. """
 from typing import List
+import asyncio
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    '''Executes task_wait_random n times.
-    '''
-    wait_times = await asyncio.gather(
-        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
+    """ returns list of all delays """
+    wait_list = await asyncio.gather(
+        *tuple(map(lambda _: wait_random(max_delay), range(n)))
     )
-    return sorted(wait_times)
+    return sorted(wait_list)
